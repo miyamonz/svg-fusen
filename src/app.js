9131b5d5cbacc0699bc58186f3dc57ca58ba0bc0
Koichi Miyamoto <m5813k@gmail.com>
2019-08-04 01:31:55 +0900

    waketa

--- src/app.js
+++ src/app.js
@@ -1,52 +1,23 @@
 import React, { useState, useEffect } from "react";
+import useCreateRect from "./useCreateRect";
 
-const getXY = e => ({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY });
-const createRect = a => b => {
-  const width = Math.abs(a.x - b.x);
-  const height = Math.abs(a.y - b.y);
-
-  const x = Math.min(a.x, b.x);
-  const y = Math.min(a.y, b.y);
-
-  return { x, y, width, height };
-};
-
-function useCreateRect() {
-  const [rect, setRect] = useState({ x: 0, y: 0, width: 0, height: 90 });
-  const [mouse, setMouse] = useState(null);
-
-  const r = createRect(mouse);
-  const onMouseDown = e => {
-    const xy = getXY(e);
-    setRect({ ...xy, width: 0, height: 0 });
-    setMouse(xy);
-  };
-  const onMouseUp = e => {
-    setMouse(null);
-    setRect(r(getXY(e)));
-  };
-  const onMouseMove = e => {
-    if (mouse) setRect(r(getXY(e)));
-  };
-
-  return [{ rect, mouse }, { onMouseMove, onMouseUp, onMouseDown }];
-}
 export default function App() {
   const [{ rect, mouse }, handlers] = useCreateRect();
   const [rects, setRects] = useState([]);
 
   useEffect(
     () => {
-      if (!mouse) setRects(prev => [...prev, rect]);
+      if (!mouse && rect.width * rect.height !== 0)
+        setRects(prev => [...prev, rect]);
     },
     [mouse]
   );
   return (
     <svg width="100%" height="100%" {...handlers}>
-      {mouse && <rect {...rect} fill="red" />}
       {rects.map(r => (
-        <rect key={JSON.stringify(r)} {...r} stroke="black" fill="none" />
+        <rect key={JSON.stringify(r)} {...r} stroke="black" fill="lightgray" />
       ))}
+      {mouse && <rect {...rect} fill="red" />}
     </svg>
   );
 }
---
+++ src/useCreateRect.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+
+const getXY = e => ({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY });
+const createRect = a => b => {
+  const width = Math.abs(a.x - b.x);
+  const height = Math.abs(a.y - b.y);
+
+  const x = Math.min(a.x, b.x);
+  const y = Math.min(a.y, b.y);
+
+  return { x, y, width, height };
+};
+
+export default function useCreateRect() {
+  const [rect, setRect] = useState({ x: 0, y: 0, width: 0, height: 90 });
+  const [mouse, setMouse] = useState(null);
+
+  const r = createRect(mouse);
+  const onMouseDown = e => {
+    console.log(e.target.tagName);
+    if (e.target.tagName !== "svg") {
+      return;
+    }
+    const xy = getXY(e);
+    setRect({ ...xy, width: 0, height: 0 });
+    setMouse(xy);
+  };
+  const onMouseUp = e => {
+    if (!mouse) return;
+    setMouse(null);
+    setRect(r(getXY(e)));
+  };
+  const onMouseMove = e => {
+    if (mouse) setRect(r(getXY(e)));
+  };
+
+  return [{ rect, mouse }, { onMouseMove, onMouseUp, onMouseDown }];
+}
